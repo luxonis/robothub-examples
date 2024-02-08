bf5497a1ec36a428f48ebe521d5786cd84fcff51
Radim Krestan <radim.krestan@juicymo.cz>
2024-02-08 16:49:30 +0100

    Refactor architecture of the app

--- Applications/people_reid_and_stats/recorder.py
+++ Applications/people_reid_and_stats/recorder.py
@@ -6,6 +6,8 @@ import uuid
 from collections import deque
 from pathlib import Path
 
+from robothub.events import send_video_event
+
 from base_node import BaseNode
 from depthai_sdk.recorders.video_writers import AvWriter
 from messages import Person, PeopleFacesMessage
@@ -42,24 +44,35 @@ class Recorder(BaseNode):
         thread.start()
 
     def __record_video_thread(self):
-        record_id = str(uuid.uuid4())
-        self.__timer.update_timestamp(event=record_id)
-        log.info(f"Recording {record_id}")
-        while not self.__timer.event_time_elapsed(event=record_id, seconds=60 * CONFIGURATION["recording_length"]):
+        self._record_id = str(uuid.uuid4())
+        self.__timer.update_timestamp(event=self._record_id)
+        log.info(f"Recording {self._record_id}")
+        while not self.__timer.event_time_elapsed(event=self._record_id,
+                                                  seconds=60 * CONFIGURATION["recording_length"]):
             time.sleep(1.)
-        local_storage = LocalStorage(record_id=record_id, save_function=self.__save_video)
-        local_storage.handle_video_saving()
+        self.__save_video()
 
-    def __save_video(self, name: str, dir_path: Path) -> Path:
-        av_writer = AvWriter(path=Path(dir_path),
-                             name=name,
+    def __save_video(self) -> None:
+        self._local_storage = LocalStorage(file_name=self._record_id, file_suffix='.mp4',
+                                           subdir_path=CONFIGURATION["video_storage_location"],
+                                           gib_storage_limit=CONFIGURATION["storage_space_limit"])
+        av_writer = AvWriter(path=self._local_storage.get_dir_path(),
+                             name=self._record_id,
                              fourcc='h264',
                              fps=CONFIGURATION["fps"],
                              frame_shape=(IMAGE_WIDTH, IMAGE_HEIGHT))
         packets = list(self.__buffer)
         for p in packets:
             av_writer.write(p)
-
         av_writer.close()
-        video_path = Path(dir_path, name).with_suffix('.mp4')
-        return video_path
+
+        self._handle_video_saving(video_path=self._local_storage.get_video_path())
+
+    def _handle_video_saving(self, video_path: Path) -> None:
+        log.info(f"Storing image remotely")
+        send_video_event(video=video_path.as_posix(), title=f"Recording {self._record_id}")
+        if CONFIGURATION["local_storage_enabled"]:
+            log.info(f"Storing image locally")
+            self._local_storage.manage_stored_video(remove_oldest_enabled=CONFIGURATION["remove_oldest_enabled"])
+        else:
+            video_path.unlink()
--- Applications/people_reid_and_stats/utilities/local_storage.py
+++ Applications/people_reid_and_stats/utilities/local_storage.py
@@ -2,62 +2,57 @@ import os
 import logging as log
 
 from pathlib import Path
-from robothub_core import CONFIGURATION
-from robothub.events import send_video_event
 
 
 class LocalStorage:
-    """ LocalStorage module needs an implementation of save function that returns a path to stored video.
+    """ LocalStorage module needs dir_path which handles where data should be stored to
     If local storage is active id doesn't delete the file where the video is stored.
     """
-    def __init__(self, record_id, save_function):
-        self._dir_path = LocalStorage.__create_dir_path()
-        self._record_id = record_id
-        self._save_function = save_function
+    def __init__(self, file_name: str, gib_storage_limit: int, file_suffix: str,
+                 storage_path=Path(f'/shared'), subdir_path="robothub-media"):
+        self._dir_path = self._create_dir_path(storage_path, subdir_path)
+        self._video_path = self._create_video_path(file_name, file_suffix)
+        self._storage_limit = gib_storage_limit
 
-    def handle_video_saving(self) -> None:
-        if CONFIGURATION["local_storage_enabled"]:
-            log.info(f"Storing image locally")
-            self.__store_locally()
-        else:
-            log.info(f"Storing image remotely")
-            self.__store_on_cloud()
+    def get_dir_path(self) -> Path:
+        return self._dir_path
 
-    def __store_locally(self) -> None:
-        video_path = self._save_function(name=self._record_id, dir_path=self._dir_path)
-        if self.__is_storage_full():
-            if CONFIGURATION["remove_oldest_enabled"]:
+    def get_video_path(self):
+        return self._video_path
+
+    def manage_stored_video(self, remove_oldest_enabled=False) -> None:
+        if self._is_storage_full():
+            if remove_oldest_enabled:
                 log.info(f"Removing oldest image")
-                self.__remove_oldest_videos()
+                self._remove_oldest_videos()
             else:
-                video_path.unlink()
+                self._video_path.unlink()
                 log.info(f"Remove oldest video is disabled so the recording has been deleted")
                 return
-        log.info(f"Recording saved to {video_path}")
+        log.info(f"Recording saved to {self._video_path}")
 
-    def __store_on_cloud(self) -> None:
-        video_path = self._save_function(name=self._record_id, dir_path=self._dir_path)
-        send_video_event(video=video_path.as_posix(), title=f"Recording {self._record_id}")
-        video_path.unlink()
-
-    def __remove_oldest_videos(self) -> None:
-        while self.__is_storage_full():
+    def _remove_oldest_videos(self) -> None:
+        while self._is_storage_full():
             files = self._dir_path.glob('*')
             oldest_file = min(files, key=lambda x: x.stat().st_ctime)
             oldest_file.unlink()
             log.info(f"Removed oldest video")
 
-    def __is_storage_full(self) -> bool:
+    def _is_storage_full(self) -> bool:
         # 1 GIB = 1073741824 bytes
-        return CONFIGURATION["storage_space_limit"] <= (self.__calculate_dir_size() / 1073741824)
+        return self._storage_limit <= (self._calculate_dir_size(dir_path=self._dir_path) / 1073741824)
 
-    def __calculate_dir_size(self) -> int:
-        for root, dirs, files in os.walk(self._dir_path):
+    def _create_video_path(self, file_name: str, file_suffix: str) -> Path:
+        return Path(self._dir_path, file_name).with_suffix(file_suffix)
+
+    @staticmethod
+    def _calculate_dir_size(dir_path) -> int:
+        for root, dirs, files in os.walk(dir_path):
             return sum(os.path.getsize(os.path.join(root, name)) for name in files)
 
     @staticmethod
-    def __create_dir_path() -> Path:
-        subdir = CONFIGURATION["video_storage_location"].lstrip(' /')
-        dir_path = Path(f'/shared') / subdir
+    def _create_dir_path(file_path: Path, subdir: str) -> Path:
+        subdir = subdir.lstrip(' /')
+        dir_path = file_path / subdir
         dir_path.mkdir(parents=True, exist_ok=True)
         return dir_path
