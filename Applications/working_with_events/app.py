0b1e423099e6f87805bf72c44b6f7ab5342960eb
jan-stepan <jan.stepan@luxonis.com>
2024-03-05 12:16:52 +0100

    event manipulation and live view

--- Applications/working_with_events/app.py
+++ Applications/working_with_events/app.py
@@ -1,8 +1,15 @@
+import json
 import time
-
-import depthai
-import depthai as dai
 from pathlib import Path
+
+# cv2 and av bug workaround
+import cv2
+import numpy as np
+
+cv2.imshow("bugfix", np.zeros((10, 10, 3), dtype=np.uint8))
+cv2.destroyWindow("bugfix")
+
+import depthai as dai
 
 import logging as log
 import robothub as rh
@@ -16,35 +23,45 @@ class Application(rh.BaseDepthAIApplication):
     def __init__(self):
         # App
         super().__init__()
+        self.live_view = rh.DepthaiLiveView(name="live_view", unique_key="rgb", width=1920, height=1080)
 
     def setup_pipeline(self) -> dai.Pipeline:
         pipeline = dai.Pipeline()
         create_pipeline(pipeline=pipeline, config=self.config)
         return pipeline
 
-    def manage_device(self, device: depthai.Device):
+    def manage_device(self, device: dai.Device):
         log.info(f"DepthAi version: {dai.__version__}")
         log.info(f"Oak started. getting queues...")
-        rgb_preview = device.getOutputQueue(name="rgb_preview", maxSize=5, blocking=False)
+        rgb_h264 = device.getOutputQueue(name="rgb_h264", maxSize=5, blocking=False)
         rgb_mjpeg = device.getOutputQueue(name="rgb_mjpeg", maxSize=5, blocking=False)
         detection_nn = device.getOutputQueue(name="detection_nn", maxSize=5, blocking=False)
+        # TODO add stereo output queue
 
         node = self.pipeline.getNode(0)
         print(node)
 
-        live_view = rh.LiveView.create(component=self.pipeline.getNode(), name="Color stream")
+        model_config = Path("nn_models/detection_config.json")
+        with model_config.open() as f:
+            config = json.loads(f.read())
+
         while self.running:
-            rgb_frame: dai.ImgFrame = rgb_preview.get()
+            rgb_h264_frame: dai.ImgFrame = rgb_h264.get()
             rgb_mjpeg_frame: dai.ImgFrame = rgb_mjpeg.get()
             detections: dai.ImgDetections = detection_nn.get()
+            # TODO get stereo ImgFrame
             for detection in detections.detections:
                 if detection.label == 0:
-                    frame_size = rgb_frame.getWidth(), rgb_frame.getHeight()
+                    frame_size = rgb_h264_frame.getWidth(), rgb_h264_frame.getHeight()
                     bbox = int(detection.xmin * frame_size[0]), int(detection.ymin * frame_size[1]), int(
                         detection.xmax * frame_size[0]), int(detection.ymax * frame_size[1])
-                    live_view.add_rectangle(bbox, label=self.config["mappings"]["labels"][0])
-                    rh.events.send_image_event(image=rgb_mjpeg_frame.getFrame(), title="Person detected", device_id=device.getMxId())
+                    self.live_view.add_rectangle(bbox, label=config["mappings"]["labels"][0])
+                    rh.events.send_image_event(image=rgb_mjpeg_frame.getFrame(), title="Person detected",
+                                               device_id=device.getMxId())
+            # TODO publish stereo output with the frame side by side
+            self.live_view.publish(h264_frame=rgb_h264_frame.getFrame())
             time.sleep(0.01)
+
 
 if __name__ == '__main__':
     app = Application()
--- Applications/working_with_events/pipeline.py
+++ Applications/working_with_events/pipeline.py
@@ -6,21 +6,23 @@ from depthai_sdk.components.nn_helper import Path
 
 def create_pipeline(pipeline: dai.Pipeline, config: dict):
     rgb_sensor = create_rgb_sensor(pipeline, fps=config["fps"])
+    # TODO add left(CAM_B) and right(CAM_C) monocamera
+    # TODO add stereo depth
     rgb_input = pipeline.createXLinkIn()
     rgb_input.setStreamName("rgb_input")
     rgb_input.out.link(rgb_sensor.inputControl)
     rgb_h264_encoder = create_h264_encoder(pipeline=pipeline, fps=config["fps"])
     rgb_mjpeg_encoder = create_mjpeg_encoder(pipeline=pipeline, fps=config["fps"])
     # link
-
     rgb_sensor.video.link(rgb_h264_encoder.input)
     rgb_sensor.video.link(rgb_mjpeg_encoder.input)
     image_manip = create_image_manip(pipeline=pipeline, source=rgb_sensor.preview, resize=(640, 640))
     detection_nn = create_detecting_nn(pipeline, "nn_models/yolov6n_coco_640x640.blob", source=image_manip.out)
     # output
-    create_output(pipeline=pipeline, node=rgb_h264_encoder.bitstream, stream_name="rgb_preview")
+    create_output(pipeline=pipeline, node=rgb_h264_encoder.bitstream, stream_name="rgb_h264")
     create_output(pipeline=pipeline, node=rgb_mjpeg_encoder.bitstream, stream_name="rgb_mjpeg")
     create_output(pipeline=pipeline, node=detection_nn.out, stream_name="detection_nn")
+    # TODO add stereo output
 
 
 def create_rgb_sensor(pipeline: dai.Pipeline, fps: float) -> dai.node.ColorCamera:
@@ -55,7 +57,8 @@ def create_mjpeg_encoder(pipeline: dai.Pipeline, fps: float) -> dai.node.VideoEn
     return encoder
 
 
-def create_image_manip(pipeline: dai.Pipeline, source: dai.Node.Output, resize: tuple[int, int], keep_aspect_ration: bool = False,
+def create_image_manip(pipeline: dai.Pipeline, source: dai.Node.Output, resize: tuple[int, int],
+                       keep_aspect_ration: bool = False,
                        frame_type: dai.RawImgFrame.Type = dai.RawImgFrame.Type.BGR888p, output_frame_dims: int = 3,
                        blocking_input_queue: bool = False, input_queue_size: int = 4, frames_pool: int = 4,
                        wait_for_config: bool = False) -> dai.node.ImageManip:
@@ -73,7 +76,7 @@ def create_image_manip(pipeline: dai.Pipeline, source: dai.Node.Output, resize: 
 
 
 def create_detecting_nn(pipeline: dai.Pipeline, model: str, source: dai.Node.Output) -> dai.node.NeuralNetwork:
-    model_config = Path("nn_models/object_detection_config.json")
+    model_config = Path("nn_models/detection_config.json")
     with model_config.open() as f:
         config = json.loads(f.read())
     node = pipeline.createYoloDetectionNetwork()
@@ -81,7 +84,7 @@ def create_detecting_nn(pipeline: dai.Pipeline, model: str, source: dai.Node.Out
     node.setNumClasses(1)
     node.setCoordinateSize(nn_metadata["coordinates"])
     node.setAnchors(nn_metadata["anchors"])
-    node.setAnchorMasks(nn_metadata["anchor_mask"])
+    node.setAnchorMasks(nn_metadata["anchor_masks"])
     node.setIouThreshold(nn_metadata["iou_threshold"])
     node.input.setBlocking(False)
     blob = dai.OpenVINO.Blob(Path(model).resolve())
