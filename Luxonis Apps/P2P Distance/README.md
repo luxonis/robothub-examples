320c02e3e13368377850e4ecb48fe28eade1a2b8
Han Bui <ha@sparkenv.com>
2024-07-26 11:37:19 +0200

    initial commit - very simple POC

---
+++ Luxonis Apps/P2P Distance/README.md
@@ -0,0 +1,28 @@
+# P2P Distance Measurement
+
+## Description
+This repository contains an application built on top of [DepthAI](https://docs.luxonis.com/software/). This Python script captures depth data from a stereo camera setup and allows the user to click on two points in the captured image to calculate and display the distance between them.
+
+## Prerequisites
+
+- Python 3.x
+- OpenCV 
+- DepthAI
+- NumPy
+
+## Usage
+1. Run the python script `app_poc.py` 
+2. **Select Points**: Click on the video window to select two points. The distance will be displayed at a line between them.
+3. Press 'q' to exit.
+
+## Method for Calculating Distance
+### Euclidean formula 
+$$
+\text{distance} = \sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2 + (z_2 - z_1)^2}
+$$
+
+where $x, y, z$ are the coordinates of the two selected points. $z$ is taken from the depth map, coordinates $x$ and $y$ are in pixels and is converted to cm as follows:
+
+$$
+\text{cm\_per\_px} = \frac{2 \cdot z \cdot \tan\left(\frac{\text{HFOV}}{2} \cdot \frac{\pi}{180}\right)}{\text{HPixels}}
+$$
---
+++ Luxonis Apps/P2P Distance/app_poc.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+import depthai as dai
+
+class PointDistanceDrawer:
+    pointColor = (0, 0, 255)
+    lineColor = (200, 0, 200)
+    circleRadius = 2
+
+    def __init__(self):
+        self.points = []
+        self.distance = -1
+
+    def click_event(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if len(self.points) == 2:
+                self.points.clear()
+            # print(f"x: {x}, y: {y}, z: {param['depthFrame'][y, x] / 10.0:.2f} cm")
+            self.points.append((x, y))
+            if len(self.points) == 2:
+                self.distance = param['distance_calculator'].calculate_distance(self.points, param['depthFrame'], param['frame'])
+
+    def draw(self, img):
+        self.draw_point(img)
+        self.draw_line(img)
+
+    def draw_point(self, img):
+        for point in self.points:
+            cv2.circle(img, point, self.circleRadius, self.pointColor, -1, cv2.LINE_AA, 0)
+
+    def draw_line(self, img):
+        if len(self.points) != 2:
+            return
+        p1, p2 = self.points
+        distance = self.distance
+        cv2.line(img, p1, p2, self.lineColor, 1, cv2.LINE_AA, 0)
+
+        # First, draw the border in black
+        cv2.putText(img, f"{distance:.2f} cm", 
+                    ((p1[0] + p2[0]) // 2, (p1[1] + p2[1]) // 2),
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.5, 
+                    (0, 0, 0), 
+                    2, 
+                    cv2.LINE_AA)
+
+        # Then, draw the inner text in white
+        cv2.putText(img, f"{distance:.2f} cm", 
+                    ((p1[0] + p2[0]) // 2, (p1[1] + p2[1]) // 2),
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.5, 
+                    (255, 255, 255), 
+                    1, 
+                    cv2.LINE_AA)
+
+class DistanceCalculator:
+    hfov = None
+    image_w = None
+
+    def __init__(self, hfov, image_w):
+        self.hfov = hfov
+        self.image_w = image_w
+
+    def convert_pixel_to_cm(self, z):
+        view_width_cm = 2 * z * np.tan(np.radians(self.hfov / 2))
+        return view_width_cm / self.image_w
+
+    def calculate_distance(self, points, depthFrame, frame):
+        if len(points) != 2:
+            return -1
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        depth1 = depthFrame[y1, x1] / 1000.0
+        depth2 = depthFrame[y2, x2] / 1000.0
+
+        if depth1 == 0 or depth2 == 0:
+            print("Invalid depth")
+            return -1
+            
+        cm_per_px_p1 = self.convert_pixel_to_cm(depth1)
+        cm_per_px_p2 = self.convert_pixel_to_cm(depth2)
+
+        x1_cm = x1 * cm_per_px_p1
+        y1_cm = y1 * cm_per_px_p1
+        x2_cm = x2 * cm_per_px_p2
+        y2_cm = y2 * cm_per_px_p2
+
+        # 3D Euclidean distance 
+        dist = np.sqrt((x2_cm - x1_cm)**2 + (y2_cm - y1_cm)**2 + (depth2 - depth1)**2)
+        
+        # Convert from m to cm 
+        dist *= 100
+        return dist
+
+
+LR_CHECK = True
+EXTENDED = False # extended disparity for lowering minimal distance for depth calculation
+MEDIAN = dai.MedianFilter.KERNEL_7x7
+SUBPIXEL = False # for long range measurement
+
+pipeline = dai.Pipeline()
+
+# define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+monoCams = [monoLeft, monoRight]
+
+stereo = pipeline.create(dai.node.StereoDepth)
+
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+xoutRectifLeft = pipeline.create(dai.node.XLinkOut)
+
+xoutDepth.setStreamName("depth")
+xoutRectifLeft.setStreamName("rectifiedLeft")
+
+# properties
+for monoCam in monoCams:
+    monoCam.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+stereo.initialConfig.setMedianFilter(MEDIAN)
+stereo.initialConfig.setConfidenceThreshold(250)
+stereo.setLeftRightCheck(LR_CHECK)
+stereo.setExtendedDisparity(EXTENDED)
+stereo.setSubpixel(SUBPIXEL)
+
+config = stereo.initialConfig.get()
+config.postProcessing.speckleFilter.enable = True
+config.postProcessing.speckleFilter.speckleRange = 50
+config.postProcessing.temporalFilter.enable = False
+config.postProcessing.spatialFilter.enable = True
+config.postProcessing.spatialFilter.holeFillingRadius = 2
+config.postProcessing.spatialFilter.numIterations = 1
+config.postProcessing.thresholdFilter.minRange = 400
+config.postProcessing.thresholdFilter.maxRange = 15000
+config.postProcessing.decimationFilter.decimationFactor = 1
+stereo.initialConfig.set(config)
+
+# link
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+stereo.depth.link(xoutDepth.input)
+stereo.rectifiedLeft.link(xoutRectifLeft.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+    qDepth = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+    qRectifLeft = device.getOutputQueue(name="rectifiedLeft", maxSize=4, blocking=False)
+
+    drawer = PointDistanceDrawer()
+    distance_calculator = DistanceCalculator(71.86, 640)
+
+    while True:
+        inDepth = qDepth.get()
+        depthFrame = inDepth.getFrame()
+
+        inRectifLeft = qRectifLeft.get()
+        rectifLeftFrame = cv2.cvtColor(inRectifLeft.getFrame(), cv2.COLOR_GRAY2BGR)
+
+        cvColorMap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_JET)
+        depthFrameColored = cv2.applyColorMap((depthFrame * (255.0 / stereo.initialConfig.getMaxDisparity())).astype(np.uint8), cvColorMap)
+
+        cv2.setMouseCallback("rectifiedLeft", drawer.click_event, {'depthFrame': depthFrame, 'frame': rectifLeftFrame, 'distance_calculator': distance_calculator})
+        distance_calculator.calculate_distance(drawer.points, depthFrame, rectifLeftFrame)
+        drawer.draw(rectifLeftFrame)
+        cv2.imshow("rectifiedLeft", rectifLeftFrame)
+
+        cv2.imshow("depth", depthFrameColored)
+
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cv2.destroyAllWindows()
